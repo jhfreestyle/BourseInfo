5bb6c914d224b37f739c3ff882360f5968c0be49
Jean Gizard <jgizard@efront.com>
2018-11-09 11:59:15 +0100

    Add unit test Framework + 1 test

---
+++ BourseInfoTests/MainFormTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using BourseInfo;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BourseInfoTests
+{
+    [TestClass]
+    public class MainFormTests
+    {
+        [TestMethod]
+        public void HttpRequestsOnApiUrlsReturnsNotNull()
+        {
+            MainForm mf = new MainForm();
+            Stopwatch stopWatch = new Stopwatch();
+            
+
+            for (int i = 0 ; i < mf.JsonUrls.Count ; i++)
+            {
+                stopWatch.Restart();
+                var res = mf.getHttpResponse(mf.JsonUrls[i]);
+                stopWatch.Stop();
+                Assert.IsNotNull(res);
+
+                Trace.WriteLine("Url " + i + ": " + stopWatch.ElapsedMilliseconds/1000.0 + "s (" + mf.JsonUrls[i] + ")");
+            }
+            
+        }
+    }
+}
---
+++ BourseInfoTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BourseInfoTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("eFront")]
+[assembly: AssemblyProduct("BourseInfoTests")]
+[assembly: AssemblyCopyright("Copyright © eFront 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0579dab4-e109-481b-b360-8d426356ed69")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
