3297af251769ec55fe2e30b980b1ad01791afc45
Gizard Jean <jgizard@efront.Com>
2019-01-22 13:55:17 +0100

    Add Portfolio constructor. Add property Info in Stock class.

--- PortfolioManagement/Portfolio.cs
+++ PortfolioManagement/Portfolio.cs
@@ -7,13 +7,23 @@ using System.Threading.Tasks;
 
 namespace PortfolioManagement
 {
-    public class Portfolio
+    public partial class Portfolio
     {
         public List<Transaction> Ledger;
 
+        public Portfolio()
+        {
+            Ledger = new List<Transaction>();
+        }
+
         public decimal GetPortfolioValue()
         {
-            return Ledger.Sum(t => t.Quantity * t.Stock.Value);
+            return Ledger.Sum(t => t.Stock?.Value ?? 0);
+        }
+
+        public void AddTransaction(Stock stock, int quantity, decimal price)
+        {
+            Ledger.Add(new Transaction(stock, quantity, price));
         }
 
     }
@@ -41,6 +51,7 @@ namespace PortfolioManagement
         public string Ticker { get; set; }
         public decimal Value { get; set; }
         public decimal Pct { get; set; }
+        public string Info { get; set; }
 
     }
 
